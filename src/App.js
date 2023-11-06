81319ada4f68e2e4560a9536b790ae74d3bb713f
petridisraf <146370490+petridisraf@users.noreply.github.com>
2023-11-06 15:32:02 +0200

    commit

--- src/App.js
+++ src/App.js
@@ -6,23 +6,46 @@ function App() {
     <div className="App">
       <header className="App-header">
       <div class="container">
-        <div class="cover-photo">
-          <div class="graph"></div>
-          <ul class="timeline"></ul>
-        </div>
-        <div class="visallo"></div>
-        <h1 class="tagline">The power to <span>illuminate</span> hidden relationships</h1>
-        <div class="logo"></div>
-        <h1 class="title">
-          V5 Analytics
-          <div>@v5analytics</div>
-        </h1>
+          <div class="row">
+              <div class="col-md-12">
+                  <div class="fb-profile-block">
+                      <div class="fb-profile-block-thumb cover-container"></div>
+                      <div class="profile-img">
+                          <a href="#">
+                              <img src="https://media.licdn.com/dms/image/D4E0BAQESoWpfWxecmQ/company-logo_200_200/0/1681324616194/kissmybutton_logo?e=2147483647&v=beta&t=MJUu74MuPZAjle9a8DlYNmfUjSH0ABUABpegjjk2ldI" alt="" title=""/>        
+                          </a>
+                      </div>
+                      
+                      
+                      <div class="fb-profile-block-menu">
+                          <div class="block-menu">
+                              <ul>
+                                  <li><a href="#">Timeline</a></li>
+                                  <li><a href="#">about</a></li>
+                                  <li><a href="#">Friends</a></li>
+                                  <li><a href="#">Photos</a></li>
+                                  <li><a href="#">More...</a></li>
+                              </ul>
+                          </div>
+                          <div class="block-menu">
+                              <ul>
+                                  <li><a href="#">Timeline</a></li>
+                                  <li><a href="#">about</a></li>
+                                  <li><a href="#">Friends</a></li>
+                                  <li><a href="#">Photos</a></li>
+                                  <li><a href="#">More...</a></li>
+                              </ul>
+                          </div>
+                      </div>
+                  </div>
+              </div>
+          </div>
       </div>
       </header>
 
       <body>
 
-
+        
       </body>
 
       <footer>
