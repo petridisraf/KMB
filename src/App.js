685e852dfe38a80a374dfe8d990a459eeab835fc
petridisraf <146370490+petridisraf@users.noreply.github.com>
2023-11-06 15:26:22 +0200

    commit

--- src/App.js
+++ src/App.js
@@ -5,12 +5,24 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        
+      <div class="container">
+        <div class="cover-photo">
+          <div class="graph"></div>
+          <ul class="timeline"></ul>
+        </div>
+        <div class="visallo"></div>
+        <h1 class="tagline">The power to <span>illuminate</span> hidden relationships</h1>
+        <div class="logo"></div>
+        <h1 class="title">
+          V5 Analytics
+          <div>@v5analytics</div>
+        </h1>
+      </div>
       </header>
 
       <body>
 
-        
+
       </body>
 
       <footer>
